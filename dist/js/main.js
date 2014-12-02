120aeeae90c2ebffa203ae918a0a894a80e4de80
Nicolas Martin <nihco2@gmail.com>
2014-12-02 06:53:29 +0100

    reword 3d39bd1 Polisse sans sÃ©rifs

---
+++ dist/js/main.js
@@ -0,0 +1,284 @@
+var stage,
+  canvas,
+  queue,
+  container,
+  ratio,
+  mc,
+  bkgHeight,
+  scrollHeight,
+  num,
+  flakes = [];
+
+//plus le pas est grand moins c'est rapide
+var PAS = 50;
+//plus c'est grand plus les assets se déplacent vite
+var ASSET_MOVEMENT = 5;
+
+var SNOW_SPEED = 3;
+
+var FLAKES_NUMBER = 100;
+
+var END_TOUCH_EVENT = 500;
+
+var FLAKES_TIME_CHANGE_DIRECTION = 20000; //miliseconds
+
+var SNOW_COLOR = {
+  red: 255,
+  green: 255,
+  blue: 255
+}
+
+var assets = [{
+  type: 'tree',
+  posX: 650,
+  posY: 2000,
+  destX: 500
+}, {
+  type: 'mountain',
+  posX: -350,
+  posY: 520,
+  destX: -300
+}, {
+  type: 'mountain',
+  posX: 350,
+  posY: 650,
+  destX: 300
+}];
+
+var isPortrait = function() {
+  if (window.innerHeight > window.innerWidth) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+var isMobile = function() {
+  return (/Mobile|Android|iPhone|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera) ? true : false;
+}
+
+function init() {
+  canvas = document.getElementById('haiku');
+  stage = new createjs.Stage("haiku");
+  ctx = canvas.getContext("2d");
+  createjs.Touch.enable(stage);
+  preloadAssets();
+  listentouchEvents();
+  if (isMobile()) {
+    $('#wrapper').width(window.innerWidth);
+    $('#wrapper').height(window.innerHeight);
+  }
+}
+
+function preloadAssets() {
+  queue = new createjs.LoadQueue();
+  queue.loadManifest([{
+    id: "bkg",
+    src: "assets/background.jpg"
+  }, {
+    id: "mountain",
+    src: "assets/montagne.png",
+  }, {
+    id: "tree",
+    src: "assets/arbre.png",
+  }], true);
+  queue.on("complete", handleComplete);
+}
+
+function handleComplete() {
+  $('.loader').remove();
+  $('.js-btn-start').addClass('block');
+  initSize();
+  initAssets();
+  stage.addChild(container);
+  window.addEventListener('resize', resize, false);
+  timer = setInterval(changeMovement, FLAKES_TIME_CHANGE_DIRECTION);
+  initSnowFlakes(SNOW_SPEED, FLAKES_NUMBER);
+  createjs.Ticker.addEventListener("tick", tick);
+}
+
+function initSize() {
+  var bkg = queue.getResult('bkg');
+  var bitmap = new createjs.Bitmap(bkg);
+  canvas.width = bkg.width;
+  canvas.height = window.innerHeight * 2;
+  container = new createjs.Container();
+  container.addChild(bitmap);
+  ratio = canvas.height / canvas.width;
+  bkgHeight = container.getBounds().height
+  $('#haiku').width(window.innerWidth).height(window.innerWidth * ratio);
+  container.y = -bkgHeight + canvas.height;
+  scrollHeight = -bkgHeight + canvas.height;
+}
+
+function initAssets() {
+  var mountain = new createjs.Bitmap(queue.getResult('mountain'));
+  var tree = new createjs.Bitmap(queue.getResult('tree'));
+  assets.forEach(function(asset, index) {
+    var item;
+    if (asset.type === 'mountain') {
+      item = mountain.clone();
+    }
+    if (asset.type === 'tree') {
+      item = tree.clone();
+    }
+    item.x = asset.posX;
+    item.y = asset.posY;
+    item.destX = asset.destX;
+    item.posX = asset.posX;
+    container.addChild(item);
+  });
+
+}
+
+function listentouchEvents() {
+  $('.js-btn-start').on('click', function() {
+    console.log('click')
+    $('header').hide();
+    $('#wrapper').show();
+  });
+  mc = new Hammer(canvas);
+  mc.get('pan').set({
+    threshold: 0
+  });
+  mc.on("panup", handlePanUp);
+  mc.on("pandown", handlePanDown);
+  canvas.addEventListener("touchend", handleEnd, false);
+}
+
+function resize() {
+  if (isMobile()) {
+    $('#wrapper').width(window.innerWidth);
+    $('#wrapper').height(window.innerHeight);
+
+    if (isPortrait()) {
+      /*container.scaleX = ratioP;
+      container.scaleY = ratioP;*/
+      container.y = -container.getBounds().height * ratioP + canvas.height + PAS;
+      scrollHeight = -container.getBounds().height * ratioP + canvas.height + PAS;
+    } else {
+      /*container.scaleX = ratioL;
+      container.scaleY = ratioL;*/
+      container.y = -container.getBounds().height * ratioL + canvas.height + PAS;
+      scrollHeight = -container.getBounds().height * ratioL + canvas.height + PAS;
+    }
+  }
+
+}
+
+function handleEnd() {
+  console.log('end event', container.y, scrollHeight);
+  if (container.y < scrollHeight + num) {
+    disablePan();
+    Tween.get(container).to({
+      y: scrollHeight
+    }, END_TOUCH_EVENT, Ease.cubicOut).call(function() {
+      enablePan();
+    });
+  }
+  if (container.y > PAS) {
+    disablePan();
+    Tween.get(container).to({
+      y: 0
+    }, END_TOUCH_EVENT, Ease.cubicOut).call(function() {
+      enablePan();
+    });
+  }
+}
+
+function disablePan() {
+  mc.set({
+    enable: false
+  });
+}
+
+
+function enablePan() {
+  mc.set({
+    enable: true
+  });
+}
+
+
+function handlePanUp(event) {
+  num = (event.deltaY * (bkgHeight - canvas.height) / canvas.height) / PAS;
+
+  if (container.y >= scrollHeight - num && container.y < -num) {
+    Tween.get(container).to({
+      y: container.y + num
+    }, 0, Ease.cubicOut);
+  }
+  moveAssetToRight(container.getChildAt(1), container.getChildAt(1).posX);
+  moveAssetToLeft(container.getChildAt(2), container.getChildAt(2).posX);
+  moveAssetToRight(container.getChildAt(3), container.getChildAt(3).posX);
+}
+
+function handlePanDown(event) {
+  num = (event.deltaY * (bkgHeight - canvas.height) / canvas.height) / PAS;
+
+  if (container.y <= -num && container.y >= scrollHeight) {
+    Tween.get(container).to({
+      y: container.y + num
+    }, 0, Ease.cubicOut);
+  }
+  moveAssetToLeft(container.getChildAt(1), container.getChildAt(1).destX);
+  if (container.y > -340) {
+    moveAssetToRight(container.getChildAt(2), container.getChildAt(2).destX);
+    moveAssetToLeft(container.getChildAt(3), container.getChildAt(3).destX);
+  }
+}
+
+function moveAssetToRight(target, max) {
+  if (target.x <= max) {
+    Tween.get(target).to({
+      x: (target.x + ASSET_MOVEMENT)
+    }, 0, Ease.cubicOut);
+  }
+}
+
+function moveAssetToLeft(target, max) {
+  if (target.x >= max) {
+    Tween.get(target).to({
+      x: (target.x - ASSET_MOVEMENT)
+    }, 0, Ease.cubicOut);
+  }
+}
+
+function initSnowFlakes(speed, flakesNumber) {
+  for (var i = 0; i < flakesNumber; i++) {
+    var g = new createjs.Graphics();
+    var flake = new createjs.Shape(g);
+    g.beginFill(createjs.Graphics.getRGB(SNOW_COLOR.red, SNOW_COLOR.green, SNOW_COLOR.blue));
+    g.drawCircle(0, 0, 3);
+    flake.vel = (Math.random() * speed) + 0.5;
+    flake.xSpeed = Math.floor(Math.random() * (0.5 - -0.5 + 1)) + -0.5;
+    flake.scaleX = (Math.random() * 1) + 0.3;
+    flake.scaleY = flake.scaleX;
+    flake.x = Math.random() * canvas.width;
+    flake.y = Math.random() * canvas.height;
+    stage.addChild(flake);
+    flakes.push(flake);
+  }
+}
+
+function fall(e) {
+  for (var i = 0; i < flakes.length; i++) {
+    flakes[i].x += flakes[i].xSpeed;
+    flakes[i].y += flakes[i].vel;
+    if (flakes[i].y > canvas.height) {
+      flakes[i].x = Math.random() * stage.getBounds().width;
+      flakes[i].y = 0;
+    }
+  }
+}
+
+function changeMovement(e) {
+  for (var i = 0; i < flakes.length; i++) {
+    flakes[i].xSpeed *= -1;
+  }
+}
+
+function tick(event) {
+  fall();
+  stage.update(event);
+}
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.configure(function () {
-  app.use(express.static(__dirname + '/')); // set the static files location
+  app.use(express.static(__dirname + '/dist')); // set the static files location
   app.use(express.logger('dev')); // log every request to the console
   app.use(express.bodyParser()); // pull information from html in POST
   app.use(express.methodOverride()); // simulate DELETE and PUT
