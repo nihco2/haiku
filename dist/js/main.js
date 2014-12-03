6362b035183e8cc90ff47e0584144112071597da
Nicolas Martin <nihco2@gmail.com>
2014-12-03 08:33:40 +0100

    update assets

--- dist/js/main.js
+++ dist/js/main.js
@@ -7,6 +7,8 @@ var stage,
   bkgHeight,
   scrollHeight,
   num,
+  gameEnabled = true,
+  collection = [],
   flakes = [];
 
 //plus le pas est grand moins c'est rapide
@@ -14,9 +16,11 @@ var PAS = 50;
 //plus c'est grand plus les assets se déplacent vite
 var ASSET_MOVEMENT = 5;
 
+var RUN_LIMIT = 800;
+
 var SNOW_SPEED = 3;
 
-var FLAKES_NUMBER = 100;
+var FLAKES_NUMBER = 70;
 
 var END_TOUCH_EVENT = 500;
 
@@ -30,17 +34,20 @@ var SNOW_COLOR = {
 
 var assets = [{
   type: 'tree',
-  posX: 650,
+  name: 'tree1',
+  comeFrom: 'right',
   posY: 2000,
   destX: 500
 }, {
   type: 'mountain',
-  posX: -350,
+  name: 'mountain1',
+  comeFrom: 'left',
   posY: 520,
   destX: -300
 }, {
   type: 'mountain',
-  posX: 350,
+  name: 'mountain2',
+  comeFrom: 'right',
   posY: 650,
   destX: 300
 }];
@@ -56,6 +63,12 @@ var isPortrait = function() {
 var isMobile = function() {
   return (/Mobile|Android|iPhone|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera) ? true : false;
 }
+
+var isOnScreen = function(el) {
+  //console.log('!!!!', el.name, el.x)
+  return (el.y >= -container.y - window.innerHeight + el.getBounds().height);
+};
+
 
 function init() {
   canvas = document.getElementById('haiku');
@@ -76,6 +89,18 @@ function preloadAssets() {
     id: "bkg",
     src: "assets/background.jpg"
   }, {
+    id: "spring",
+    src: "assets/bg_printemps.jpg"
+  }, {
+    id: "winter",
+    src: "assets/bg_hiver.jpg"
+  }, {
+    id: "autumn",
+    src: "assets/bg_automne.jpg"
+  }, {
+    id: "summer",
+    src: "assets/bg_ete.jpg"
+  }, {
     id: "mountain",
     src: "assets/montagne.png",
   }, {
@@ -88,22 +113,41 @@ function preloadAssets() {
 function handleComplete() {
   $('.loader').remove();
   $('.js-btn-start').addClass('block');
+  initSeasons();
   initSize();
   initAssets();
-  stage.addChild(container);
+
   window.addEventListener('resize', resize, false);
   timer = setInterval(changeMovement, FLAKES_TIME_CHANGE_DIRECTION);
   initSnowFlakes(SNOW_SPEED, FLAKES_NUMBER);
   createjs.Ticker.addEventListener("tick", tick);
 }
 
-function initSize() {
-  var bkg = queue.getResult('bkg');
-  var bitmap = new createjs.Bitmap(bkg);
-  canvas.width = bkg.width;
-  canvas.height = window.innerHeight * 2;
+function initSeasons() {
+  var seasons = [];
+  seasons[0] = queue.getResult('winter'),
+  seasons[1] = queue.getResult('summer'),
+  seasons[2] = queue.getResult('autumn'),
+  seasons[3] = queue.getResult('spring');
+
   container = new createjs.Container();
+  seasons.forEach(function(season, index) {
+    var bitmap = new createjs.Bitmap(season);
+    container.addChild(bitmap);
+    (index > 0) ? bitmap.y = container.getBounds().height : bitmap.y = 0;
+    bitmap.visible = false;
+  })
+  bitmap = new createjs.Bitmap(queue.getResult('bkg'));
   container.addChild(bitmap);
+  stage.addChild(container);
+
+}
+
+function initSize() {
+
+  canvas.width = container.getBounds().width;
+  canvas.height = window.innerHeight * 2;
+
   ratio = canvas.height / canvas.width;
   bkgHeight = container.getBounds().height
   $('#haiku').width(window.innerWidth).height(window.innerWidth * ratio);
@@ -121,19 +165,25 @@ function initAssets() {
     }
     if (asset.type === 'tree') {
       item = tree.clone();
+      item.scaleX = item.scaleY = 1.5;
     }
-    item.x = asset.posX;
+    if (asset.comeFrom === 'right') {
+      item.x = canvas.width;
+    } else {
+      item.x = -item.getBounds().width;
+    }
+    item.name = asset.name;
+    item.comeFrom = asset.comeFrom;
     item.y = asset.posY;
     item.destX = asset.destX;
-    item.posX = asset.posX;
+    item.posX = item.x;
+    collection.push(item);
     container.addChild(item);
   });
-
 }
 
 function listentouchEvents() {
   $('.js-btn-start').on('click', function() {
-    console.log('click')
     $('header').hide();
     $('#wrapper').show();
   });
@@ -145,6 +195,16 @@ function listentouchEvents() {
   mc.on("pandown", handlePanDown);
   canvas.addEventListener("touchend", handleEnd, false);
 }
+
+function scrollToBottom() {
+  Tween.get(container).to({
+    y: scrollHeight
+  }, END_TOUCH_EVENT, Ease.cubicOut).call(function() {
+    enablePan();
+    gameEnabled = true;
+  });
+}
+
 
 function resize() {
   if (isMobile()) {
@@ -167,14 +227,10 @@ function resize() {
 }
 
 function handleEnd() {
-  console.log('end event', container.y, scrollHeight);
+  //console.log('end event', container.y, scrollHeight);
   if (container.y < scrollHeight + num) {
     disablePan();
-    Tween.get(container).to({
-      y: scrollHeight
-    }, END_TOUCH_EVENT, Ease.cubicOut).call(function() {
-      enablePan();
-    });
+    scrollToBottom();
   }
   if (container.y > PAS) {
     disablePan();
@@ -208,24 +264,43 @@ function handlePanUp(event) {
       y: container.y + num
     }, 0, Ease.cubicOut);
   }
-  moveAssetToRight(container.getChildAt(1), container.getChildAt(1).posX);
-  moveAssetToLeft(container.getChildAt(2), container.getChildAt(2).posX);
-  moveAssetToRight(container.getChildAt(3), container.getChildAt(3).posX);
+  moveAssets('down');
 }
 
 function handlePanDown(event) {
   num = (event.deltaY * (bkgHeight - canvas.height) / canvas.height) / PAS;
-
+  if (event.deltaY > RUN_LIMIT && gameEnabled) {
+    disablePan();
+    scrollToBottom();
+    gameEnabled = false;
+    return alert('Rien ne sert de courir petit scarabée !');
+  }
   if (container.y <= -num && container.y >= scrollHeight) {
     Tween.get(container).to({
       y: container.y + num
     }, 0, Ease.cubicOut);
   }
-  moveAssetToLeft(container.getChildAt(1), container.getChildAt(1).destX);
-  if (container.y > -340) {
-    moveAssetToRight(container.getChildAt(2), container.getChildAt(2).destX);
-    moveAssetToLeft(container.getChildAt(3), container.getChildAt(3).destX);
-  }
+  moveAssets('up');
+}
+
+function moveAssets(vertical) {
+  collection.forEach(function(item) {
+    //console.log(item.name, isOnScreen(item, st));
+    if (isOnScreen(item) && vertical === 'up') {
+      if (item.comeFrom === 'right') {
+        moveAssetToLeft(item, item.destX);
+      } else {
+        moveAssetToRight(item, item.destX);
+      }
+    }
+    if (isOnScreen(item) && vertical === 'down') {
+      if (item.comeFrom === 'right') {
+        moveAssetToRight(item, item.posX);
+      } else {
+        moveAssetToLeft(item, item.posX);
+      }
+    }
+  })
 }
 
 function moveAssetToRight(target, max) {
